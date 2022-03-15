43275096f277b8ff56a6fabd0cf0a18269db3eb3
Ballazi <balram17dec1999@gmail.com>
2022-03-15 13:46:16 +0530

    data table

---
+++ src/component/DataTable.js
@@ -0,0 +1,146 @@
+import React, { useMemo } from 'react';
+import { useTable, useGlobalFilter, useSortBy, useFilters, usePagination } from 'react-table'
+import ArrayOfItem from '../modal/ArrayOfItem';
+import Col from '../modal/Col';
+import ColumnFilter from './ColumnFilter';
+import GlobalFilter from './GlobalFilter';
+
+const DataTable = () => {
+
+    const data = useMemo(() => ArrayOfItem, [])
+    const columns = useMemo(() => Col, [])
+    const defaultColumn = React.useMemo(
+        () => ({
+            // Let's set up our default Filter UI
+            Filter: ColumnFilter,
+        }),
+        []
+    )
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        page,
+        nextPage,
+        previousPage,
+        canNextPage,
+        canPreviousPage,
+        pageOptions,
+        gotoPage,
+        pageCount,
+        setPageSize,
+        state,
+        setGlobalFilter,
+        prepareRow,
+    } = useTable({ columns, data, defaultColumn }, useFilters, useGlobalFilter, useSortBy, usePagination)
+
+    const { globalFilter, pageIndex, pageSize } = state;
+
+
+    return (
+        <div style={{ width: "80%", margin: "10px auto", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+            <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+            <table cellSpacing={0} cellPadding={2} {...getTableProps()} style={{ border: 'solid 1px blue' }}>
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
+                            {headerGroup.headers.map(column => (
+                                <th
+                                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                                    style={{
+                                        borderBottom: 'solid 3px red',
+                                        background: 'aliceblue',
+                                        color: 'black',
+                                        fontWeight: 'bold',
+                                    }}
+                                >
+                                    {column.render('Headers')}
+                                    <span>
+                                        {column.isSorted
+                                            ? column.isSortedDesc
+                                                ? ' 🔽'
+                                                : ' 🔼'
+                                            : ''}
+                                    </span>
+                                    <div>{column.canFilter ? column.render('Filter') : null}</div>
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {page.map(row => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return (
+                                        <td
+                                            {...cell.getCellProps()}
+                                            style={{
+                                                padding: '10px',
+                                                border: 'solid 1px gray',
+                                                background: 'papayawhip',
+                                            }}
+                                        >
+                                            {cell.render('Cell')}
+                                        </td>
+                                    )
+                                })}
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+            <div style={{padding: "0.5rem"}}>
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                </button>{' '}
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    {'<'}
+                </button>{' '}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                    {'>'}
+                </button>{' '}
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    {'>>'}
+                </button>{' '}
+                <span>
+                    Page{' '}
+                    <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </strong>{' '}
+                </span>
+                <span>
+                    | Go to page:{' '}
+                    <input
+                        type="number"
+                        min="1"
+                        max={pageCount}
+                        defaultValue={pageIndex + 1}
+                        onChange={e => {
+                            const page = e.target.value ? Number(e.target.value) - 1 : 0
+                            gotoPage(page)
+                        }}
+                        style={{ width: '100px' }}
+                    />
+                </span>{' '}
+                <select
+                    value={pageSize}
+                    onChange={e => {
+                        setPageSize(Number(e.target.value))
+                    }}
+                >
+                    {[5, 10, 20, 30, 40, 50].map(pageSize => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </div>
+    )
+}
+
+export default DataTable
---
+++ src/modal/Col.js
@@ -0,0 +1,23 @@
+
+const Col =
+    [
+        {
+            Headers: "SI. No.",
+            accessor: "id",
+        },
+        {
+            Headers: "Name",
+            accessor: "name",
+        },
+        {
+            Headers: "Age",
+            accessor: "age",
+        },
+        {
+            Headers: "Gender",
+            accessor: "gender",
+        },
+
+    ]
+
+export default Col
